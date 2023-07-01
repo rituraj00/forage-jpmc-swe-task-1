050a3a2629db43d904b0257f75777da1d2e9e925
rituraj00 <rituraj171926@gmail.com>
2023-07-01 15:55:13 +0530

    Fixed methods getDataPoint, getRatio, added unit tests

--- client_test.py
+++ client_test.py
@@ -8,6 +8,11 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+       askPrice = float(quote['top_ask']['price'])
+       bidPrice = float(quote['top_bid']['price'])
+       returnedPrice = getDataPoint(quote)
+       self.assertEqual((askPrice + bidPrice) / 2, returnedPrice[-1],"Calculate price test passed!")
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +20,21 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    returnedPrice = getDataPoint(quotes[0])   
+    self.assertGreater(returnedPrice[1], returnedPrice[2], "Greater bid price than ask price")
+        
 
   """ ------------ Add more unit tests ------------ """
-
+  def test_getDataPoint_returnedPriceZero(self):
+    quotes = [
+      {'top_ask': {'price': 0, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 0, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    for quote in quotes:
+        self.assertEqual(getDataPoint(quote)[-1],0)
+           
+    
+    
 
 
 if __name__ == '__main__':
